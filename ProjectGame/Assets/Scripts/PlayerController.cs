9d57b7b7fbdb265be2f8246573ff439116fb2faa
Fractusa <56714875+Fractusa@users.noreply.github.com>
2025-09-04 17:21:32 +0200

    Created basic tilemap, added player movement
    Placeholder Player added, movement for the player has been developed. A basic tilemap has been setup, for drawing basic maps.

---
+++ ProjectGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputAction MoveAction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        MoveAction.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Reads the value of Vector2(x, y) and uses transform to add these values to the player character, giving movement
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Vector2 position = (Vector2)transform.position + move * 12.0f * Time.deltaTime;
+        transform.position = position;
+    }
+}
