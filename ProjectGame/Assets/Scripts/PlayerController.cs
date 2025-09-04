40c4a499d5391208c07fe23b804404c3c2407a3f
Fractusa <56714875+Fractusa@users.noreply.github.com>
2025-09-04 17:23:03 +0200

    Added ability to change movement speed using Unity Inspector

--- ProjectGame/Assets/Scripts/PlayerController.cs
+++ ProjectGame/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    public float movementSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,7 +17,7 @@ public class PlayerController : MonoBehaviour
     {
         //Reads the value of Vector2(x, y) and uses transform to add these values to the player character, giving movement
         Vector2 move = MoveAction.ReadValue<Vector2>();
-        Vector2 position = (Vector2)transform.position + move * 12.0f * Time.deltaTime;
+        Vector2 position = (Vector2)transform.position + move * movementSpeed * Time.deltaTime;
         transform.position = position;
     }
 }
