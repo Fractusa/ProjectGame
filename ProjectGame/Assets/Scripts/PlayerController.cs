7219d246eb67b9f6af7c5515eec38b4dab9e9bb8
Fractusa <56714875+Fractusa@users.noreply.github.com>
2025-09-06 21:07:44 +0200

    Water tiles, Water collision, Basic Health
    Added new Water tiles to tilemap, with colliders, so player can't move over water. Health value added to player.

--- ProjectGame/Assets/Scripts/PlayerController.cs
+++ ProjectGame/Assets/Scripts/PlayerController.cs
@@ -5,19 +5,35 @@ public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
     public float movementSpeed;
+    public int maxHealth = 5;
+    int currentHealth;
+    Rigidbody2D rigidbody2D;
+    Vector2 move;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Reads the value of Vector2(x, y) and uses transform to add these values to the player character, giving movement
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Vector2 position = (Vector2)transform.position + move * movementSpeed * Time.deltaTime;
-        transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2D.position + move * movementSpeed * Time.deltaTime;
+        rigidbody2D.MovePosition(position);
+    }
+
+    void Health()
+    {
+        //Makes sure that health value stays with 0 and max health.
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
     }
 }
